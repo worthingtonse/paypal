1d4173dbd262585ce2abd5f3863d72e2fc72b1c7
Sean Worthington <sean@worthington.net>
2017-04-16 18:06:29 -0700

    This does not have language support

---
+++ buy.php
@@ -0,0 +1,216 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Buy CloudCoins</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="theme-color" content="#ffffff" />
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/app.css" />
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css' />
+    <style>
+    h1, h2, h3, h4, h5, h6 {
+    font-family: &#39;Roboto&#39;, sans-serif;
+
+    color: #252c34;
+}
+.button{
+
+background-color:#338FFF;
+}
+a{
+color:#338FFF;
+font-family: &#39;Roboto&#39;, sans-serif;
+font-weight:  bold;
+}
+body{
+
+background-color:#338FFF;
+}
+    
+</style>
+    <script type="text/javascript">
+setTimeout(function(){var a=document.createElement(&quot;script&quot;);
+var b=document.getElementsByTagName(&quot;script&quot;)[0];
+a.src=document.location.protocol+&quot;//script.crazyegg.com/pages/scripts/0060/7744.js?&quot;+Math.floor(new
+Date().getTime()/3600000);
+a.async=true;a.type=&quot;text/javascript&quot;;b.parentNode.insertBefore(a,b)}, 1);
+</script>
+  </head>
+  <body>
+  <div class="top-bar"
+  style=" font-family: &#39;Roboto&#39;, sans-serif; font-size: 1.6vw; font-weight: bold; text-size-adjust:800;">
+    <div class="row">
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu=""
+        style=" font-family: &#39;Roboto&#39;, sans-serif; background-color:white;">
+          <li class="menu-text">
+            <a href="index.html">
+              <img src="img/cloudcointop.png" alt="CloudCoin logo" width="200" />
+            </a>
+          </li>
+          <li>
+            <a href="learn.html">Learn</a>
+          </li>
+          <li>
+            <a href="use.html">Use</a>
+          </li>
+          <li>Buy</li>
+          <li>
+            <a href="/news/">News</a>
+          </li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+        <ul class="menu" style=" font-family: &#39;Roboto&#39;, sans-serif; background-color:white;">
+          <li>
+            <form action="//www.findberry.com/search/" method="get" id="sr_newpage" style="display:inline">
+            <input type="hidden" name="wid" value="5222" /> 
+            <input id="sr_searchbox" type="text" name="query" value="" size="35" style="display:inline" /> 
+            <input id="sr_searchbutton" type="submit" class="button" value="Search" /></form>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <br />
+  <div class="row">
+    <div class="large-12 columns">
+      <h1 style=" font-size: 2.2vw; color:white; font-weight: bold; text-shadow: 5px 5px 15px #333333; text-size-adjust:1005"
+      class="white text-center">Todays Rate: $.011 per CloudCoin</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="large-12 columns white text-center">
+      <p>MACs and iPhones not supported yet. (software is under development).</p>
+      <p>Jpegs contain one note (33 KB). Stack (JSON text) files contain many (1 KB).</p>
+      <br />
+    </div>
+  </div>
+  
+  
+  <div class="row">
+    <div class="large-5 medium-5 columns">
+      <table>
+        <tr>
+          <th colspan="3">What format?
+          <br />Quantity is set on the next page.</th>
+        </tr>
+        <tr>
+          <td>
+            <a href="//cloudcoinconsortium.org/img/jpeg1.jpg" target="_blank" class="thumbnail">
+              <img style="max-width:none;" alt="1 CloudCoin" src="//cloudcoinconsortium.org/img/jpeg1.jpg" width="150" />
+            </a>
+          </td>
+          <th>1s</th>
+          <td>Out of Stock</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="//cloudcoinconsortium.org/img/jpeg5.jpg" target="_blank" class="thumbnail">
+              <img alt="5 CloudCoin" src="//cloudcoinconsortium.org/img/jpeg5.jpg" width="150" />
+            </a>
+          </td>
+          <th>5s</th>
+          <td>Out of Stock</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="//cloudcoinconsortium.org/img/jpeg25.jpg" target="_blank" class="thumbnail">
+              <img alt="25 CloudCoin" src="//cloudcoinconsortium.org/img/jpeg25.jpg" width="150" />
+            </a>
+          </td>
+          <th>25s</th>
+          <td>Out of Stock</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="//cloudcoinconsortium.org/img/jpeg100.jpg" target="_blank" class="thumbnail">
+              <img alt="100 CloudCoin" src="//cloudcoinconsortium.org/img/jpeg100.jpg" width="150" />
+            </a>
+          </td>
+          <th>100s</th>
+          <td>Out of Stock</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="//cloudcoinconsortium.org/img/jpeg250.jpg" target="_blank" class="thumbnail">
+              <img alt="250 CloudCoin" src="//cloudcoinconsortium.org/img/jpeg250.jpg" width="150" />
+            </a>
+          </td>
+          <th>250s
+          <br />($2.75 each)</th>
+          <th>
+      
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick" /> 
+            <input type="hidden" name="hosted_button_id" value="BRV55KB69GG44" /> 
+            <input type="image" src="http://cloudcoinconsortium.com/img/button_buy-jpegs.png" name="submit"
+            alt="PayPal - The safer, easier way to pay online!" /> 
+           
+            <img alt="PayPal" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" /></form>
+           
+       
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick" /> 
+            <input type="hidden" name="hosted_button_id" value="6CXQ9ERJ3AWHA" /> 
+            <input type="image" src="http://cloudcoinconsortium.com/img/button_buy-stack.png" name="submit"
+            alt="PayPal - The safer, easier way to pay online!" /> 
+            <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" /></form>
+          </th>
+        </tr>
+      </table>
+    </div>
+    <div class="large-5 medium-5 columns">
+      <img src="img/paypal.png" alt="PayPal" />
+      <table>
+      <tr>
+        <td>
+        <img src="img/India.png" alt="Flag of INDIA">
+        </td>
+        <td>
+        For India Support, Buy at <a href="http://cloudcoin.in/products/250s">CloudCoin.In</a>.
+        </td>
+      </tr>
+      <!--      <tr>
+        <td>
+        <img src="img/pi.png" alt="Flag of Philippines">
+        </td>
+        <td>
+        For Philippines Support, Buy at <a href="http://cloudcoins.online">CloudCoins.online</a>.
+        </td>
+      </tr>-->
+      </table>
+    </div>
+  </div>
+  <br />
+  <div class="row">
+    <div class="large-3 columns">CloudCoin Consortium &copy; 2017</div>
+    <div class="large-5 columns">Customer Service:
+    <br />1 (530) 591-7028
+    <br />CloudCoin@Protonmail.com
+    <br />We use free secure email based in Switzerland.
+    <br />
+    <a style="color:white;" href="https://protonmail.com/">Get Protonmail</a></div>
+    <div class="large-4 columns">
+    <b>Privacy Policy:</b> We do not collect user data except email addresses in case we need to send critical information.</div>
+  </div>
+  <br />
+  <br />
+  <br />
+  <script src="js/vendor/jquery.js"></script> 
+  <script src="js/vendor/what-input.js"></script> 
+  <script src="js/vendor/foundation.js"></script> 
+  <script src="js/app.js"></script></body>
+</html>
